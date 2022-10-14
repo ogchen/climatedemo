f1a7f84ece905482a304c731032728b9bb1d7b12
ogchen <oscarguoyinchen@gmail.com>
2022-10-14 17:53:06 +0100

    Initial Spring Boot skeleton

---
+++ src/main/java/com/example/climatedemo/ClimatedemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.climatedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimatedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimatedemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/climatedemo/ClimatedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.climatedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimatedemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
